e4fc9f498f7db4ff82180dc4ca439ea4f0ad2919
Remy Chantenay <remy.chantenay@carthrtottle.com>
2016-04-06 18:12:11 +0100

    CurrentApp.java added

---
+++ library/src/main/java/com/cremy/greenrobotutils/library/device/CurrentApp.java
@@ -0,0 +1,54 @@
+package com.cremy.greenrobotutils.library.device;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+/**
+ * This class allows to get some information about the current app
+ * Created by remychantenay on 15/10/2014.
+ */
+public final class CurrentApp {
+
+    /**
+     * Allows to get the current app version name
+     * (e.g. 1.54)
+     * @param _context
+     * @return 0 if context is NULL or PackageManager.NameNotFoundException raised
+     */
+    public static String getAppVersionName(Context _context) {
+        String result = "0";
+        if(_context == null) {
+            return result;
+        }
+
+        PackageInfo pInfo;
+        try {
+            pInfo = _context.getPackageManager().getPackageInfo(_context.getPackageName(), 0);
+            return pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            return result;
+        }
+    }
+
+    /**
+     * Allows to get the current app version name
+     * (e.g. 1054)
+     * @param _context
+     * @return 0 if context is NULL or PackageManager.NameNotFoundException raised
+     */
+    public static int getAppVersionCode(Context _context) {
+        int result = 0;
+        if(_context == null) {
+            return result;
+        }
+
+        PackageInfo pInfo;
+        try {
+            pInfo = _context.getPackageManager().getPackageInfo(_context.getPackageName(), 0);
+            return pInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            return result;
+        }
+    }
+}
